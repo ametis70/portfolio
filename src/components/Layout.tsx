0b9220aaf9d79c9ceca132df776bb0b61af7a2ac
Ian Mancini <ianmethyst@gmail.com>
2021-09-09 00:49:04 -0300

    Hide logo when path is not "/"

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,24 +1,25 @@
 import React, { useState, useEffect } from 'react'
-import { Image, Box, Heading, Stack } from '@chakra-ui/react'
 import { useLocation } from '@reach/router'
-import { ChakraProvider } from '@chakra-ui/react'
 import { AnimateSharedLayout, AnimatePresence } from 'framer-motion'
 
 import MotionBox from '../components/MotionBox'
-import theme from '../theme'
 import Header from './Header'
 import Canvas from './Canvas'
 
 import '@fontsource/montserrat'
+import useStore from '../store'
 
 const Layout: React.FC = ({ children }) => {
   const { pathname } = useLocation()
-  const [isHome, setHome] = useState<boolean>(pathname === '/')
+  const setHome = useStore((state) => state.setHome)
+  const isHome = useStore((state) => state.isHome)
 
   useEffect(() => {
     if (pathname === '/') setHome(true)
     else setHome(false)
   }, [pathname])
+
+  console.log(isHome)
 
   return (
     <>
