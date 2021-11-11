ed138ad28500911d691b3dfee80c6e38d572996d
Ian Mancini <ianmethyst@gmail.com>
2021-11-11 01:23:41 -0300

    Remove services/cv pages and add about page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,16 +1,18 @@
 import { PageProps } from 'gatsby'
+import { Heading } from '@chakra-ui/react'
 
 import SEO from '../components/Seo'
 
 import useMoveCamera, { cameraPositions } from '../hooks/useMoveCamera'
 
-const ServicesPage: React.FC<PageProps> = () => {
+const AboutPage: React.FC<PageProps> = () => {
   useMoveCamera(cameraPositions.FRONT)
   return (
     <>
-      <SEO title="Servicios" />
+      <SEO title="Acerca" />
+      <Heading>Acerca</Heading>
     </>
   )
 }
 
-export default ServicesPage
+export default AboutPage
