04040717e1039ebb96f1dfeea9dda1b1b80fdafe
Ian Mancini <ianmethyst@gmail.com>
2021-11-16 23:28:55 -0300

    Add color mode to cards

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,9 +1,10 @@
-import { Box } from '@chakra-ui/react'
+import { Box, useColorMode } from '@chakra-ui/react'
 
 const Card: React.FC = ({ children }) => {
+  const { colorMode } = useColorMode()
   return (
     <Box px={12} w="100%" my={8}>
-      <Box bg="amethyst.50" w="100%" px={4}>
+      <Box bg={colorMode === 'dark' ? 'amethyst.900' : 'amethyst.50'} w="100%" px={4}>
         {children}
       </Box>
     </Box>
