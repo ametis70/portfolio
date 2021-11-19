00cef7be7781dfe61e4dc01fef5045c9709f7351
Ian Mancini <ianmethyst@gmail.com>
2021-11-19 16:58:55 -0300

    Cleanup gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,16 +1,3 @@
-const path = require('path')
-// Get paths of Gatsby's required rules, which as of writing is located at:
-// https://github.com/gatsbyjs/gatsby/tree/fbfe3f63dec23d279a27b54b4057dd611dce74bb/packages/
-// gatsby/src/utils/eslint-rules
-const gatsbyRequiredRules = path.join(
-  process.cwd(),
-  'node_modules',
-  'gatsby',
-  'dist',
-  'utils',
-  'eslint-rules',
-)
-
 module.exports = {
   siteMetadata: {
     title: 'Ian Mancini',
