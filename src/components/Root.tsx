d9f67f08f4969244bd69c4e158fcb51565153610
Ian Mancini <ianmethyst@gmail.com>
2021-11-24 11:51:56 -0300

    Add react-hook-form dependency

--- src/components/Root.tsx
+++ src/components/Root.tsx
@@ -1,13 +1,18 @@
 import { I18nextProvider } from 'react-i18next'
 import { ChakraProvider } from '@chakra-ui/react'
+import { useForm, FormProvider } from 'react-hook-form'
 
 import i18n from '../i18n'
 import theme from '../theme'
 
 const Root: React.FC<{ element: JSX.Element }> = ({ element }) => {
+  const methods = useForm()
+
   return (
     <ChakraProvider theme={theme} resetCSS portalZIndex={40}>
-      <I18nextProvider i18n={i18n}>{element}</I18nextProvider>
+      <I18nextProvider i18n={i18n}>
+        <FormProvider {...methods}>{element}</FormProvider>
+      </I18nextProvider>
     </ChakraProvider>
   )
 }
