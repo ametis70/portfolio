a4235184fa43f3af599e7e260b0cdd364d7f13c6
Ian Mancini <ianmethyst@gmail.com>
2021-11-23 21:22:46 -0300

    Add skills card

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -32,82 +32,112 @@ const AboutPage: React.FC<AboutPageProps> = ({ data, pageContext }) => {
   return (
     <>
       <SEO title="Acerca" />
-      <Suspense fallback={null}>
-        <Heading variant="smallcaps" size="sectionTitle">
-          Acerca
-        </Heading>
-        <Card>
-          <Flex position="relative" left={-14} top={-8}>
-            <Box borderRadius="50%" overflow="hidden" w="fit-content" h="fit-content">
-              <StaticImage
-                src="../images/avatar.jpeg"
-                alt="Foto de Ian Mancini"
-                placeholder="blurred"
-                layout="fixed"
-                quality={90}
-                width={128}
-                height={128}
-              />
-            </Box>
-            <Stack position="relative" top={8} pl={4} py={4} spacing={1}>
-              <Heading textTransform="uppercase" fontSize="3xl">
-                {t('name')}
-              </Heading>
-              <Text>{t('job')}</Text>
-              <Box position="relative" left={-2}>
-                <SocialLinks />
-              </Box>
-            </Stack>
-          </Flex>
-        </Card>
-        <Card>
-          <Box p={6}>
-            <Markdown>{t('about')}</Markdown>
+      <Heading variant="smallcaps" size="sectionTitle">
+        Acerca
+      </Heading>
+      <Card>
+        <Flex position="relative" left={-14} top={-8}>
+          <Box borderRadius="50%" overflow="hidden" w="fit-content" h="fit-content">
+            <StaticImage
+              src="../images/avatar.jpeg"
+              alt="Foto de Ian Mancini"
+              placeholder="blurred"
+              layout="fixed"
+              quality={90}
+              width={128}
+              height={128}
+            />
           </Box>
-        </Card>
-
-        <Card>
-          <Box p={6}>
-            <Heading size="cardTitle" variant="smallcaps" as="h3">
-              {t('subtitles.experience', { ns: 'common' })}
+          <Stack position="relative" top={8} pl={4} py={4} spacing={1}>
+            <Heading textTransform="uppercase" fontSize="3xl">
+              {t('name')}
             </Heading>
-            <Divider mt={2} mb={6} />
-            <OrderedList padding={0} m={0} listStyleType="none">
-              {get<{
-                items: Array<{
-                  start: string
-                  end?: string
-                  role: string
-                  company: string
-                  description: string
-                }>
-              }>('experience').items.map((i) => (
-                <ListItem key={i.role} pb={4}>
-                  <Text fontSize="sm" variant="cardSubtitle">
-                    {new Date(i.start).getUTCFullYear()}
-                    {' — '}
-                    {i.end
-                      ? new Date(i.end).getFullYear
-                      : t('time.present', { ns: 'common' })}{' '}
+            <Text>{t('job')}</Text>
+            <Box position="relative" left={-2}>
+              <SocialLinks />
+            </Box>
+          </Stack>
+        </Flex>
+      </Card>
+
+      <Card>
+        <Box p={6}>
+          <Markdown>{t('about')}</Markdown>
+        </Box>
+      </Card>
+
+      <Card>
+        <Box p={6}>
+          <Heading size="cardTitle" variant="smallcaps" as="h3">
+            {t('subtitles.experience', { ns: 'common' })}
+          </Heading>
+          <Divider mt={2} mb={6} />
+          <OrderedList padding={0} m={0} listStyleType="none">
+            {get<{
+              items: Array<{
+                start: string
+                end?: string
+                role: string
+                company: string
+                description: string
+              }>
+            }>('experience').items.map((i) => (
+              <ListItem key={i.role} pb={4}>
+                <Text fontSize="sm" variant="cardSubtitle">
+                  {new Date(i.start).getUTCFullYear()}
+                  {' — '}
+                  {i.end
+                    ? new Date(i.end).getFullYear
+                    : t('time.present', { ns: 'common' })}{' '}
+                </Text>
+                <Box pl={4} pt={1}>
+                  <Heading size="cardTitle" as="h3">
+                    {i.role}
+                  </Heading>
+                  <Text fontSize="sm" pb={4}>
+                    {i.company}
                   </Text>
-                  <Box pl={4} pt={1}>
-                    <Heading size="cardTitle" as="h3">
-                      {i.role}
-                    </Heading>
-                    <Text fontSize="sm" pb={4}>
-                      {i.company}
-                    </Text>
-                    <Markdown>{i.description}</Markdown>
-                  </Box>
-                </ListItem>
-              ))}
-            </OrderedList>
-            <Link variant="cta" to="/works">
-              {t('ui.works_cta', { ns: 'common' })}
-            </Link>
-          </Box>
-        </Card>
-      </Suspense>
+                  <Markdown>{i.description}</Markdown>
+                </Box>
+              </ListItem>
+            ))}
+          </OrderedList>
+          <Link variant="cta" to="/works">
+            {t('ui.works_cta', { ns: 'common' })}
+          </Link>
+        </Box>
+      </Card>
+
+      <Card>
+        <Box p={6}>
+          <Heading size="cardTitle" variant="smallcaps" as="h3">
+            {t('subtitles.skills', { ns: 'common' })}
+          </Heading>
+          <Divider mt={2} mb={6} />
+          <OrderedList padding={0} m={0} listStyleType="none">
+            {get<{
+              categories: Array<{
+                name: string
+                items: Array<{ item: string }>
+              }>
+            }>('skills').categories.map((c) => (
+              <ListItem key={c.name} pb={4}>
+                <Text fontSize="sm" variant="cardSubtitle">
+                  {c.name}
+                </Text>
+                <Box pl={4} pt={1}>
+                  <Text>
+                    {c.items.map(
+                      (item, index) =>
+                        `${item.item}${index !== c.items.length - 1 ? ', ' : ''}`,
+                    )}
+                  </Text>
+                </Box>
+              </ListItem>
+            ))}
+          </OrderedList>
+        </Box>
+      </Card>
     </>
   )
 }
